b23fa8746717e7401a9e226e9576c5a918a5ef11
Graham Wood <graham@builtprecise.com>
2016-03-18 14:32:14 +0100

    Added admin field to users table Must be admin to create, edit, or delete posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :check_admin, only: [:new, :edit, :update, :destroy]
 
   def index
     @posts = Post.all
@@ -27,11 +28,17 @@ class PostsController < ApplicationController
   end
 
   private
-  def set_post
-      @post = Post.find(params[:id])
+  def check_admin
+    if !current_user.try(:admin?)
+      redirect_to "/"
     end
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
 
   def post_params
-      params.require(:post).permit(:title, :slug, :content, :date_posted)
-    end
+    params.require(:post).permit(:title, :slug, :content, :date_posted)
+  end
 end
